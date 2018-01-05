a9932ac59c14bc3bc08c70d5e22836c41b458e0a
shwethakulal <shwetha.hosangadi@gmail.com>
2018-01-05 17:47:54 +0530

    JSON parse object

---
+++ JsonObjectExample/JsonObjectExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.IO;
+
+namespace JsonObjectExample
+{
+    class colors
+    {
+        public string color { get; set; }
+        public string category { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Color: {0} \nCategory: {1}", color, category);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string JSONString = File.ReadAllText(@"data\JSONdata.json");
+
+            JavaScriptSerializer ser = new JavaScriptSerializer();
+            colors c1 = ser.Deserialize<colors>(JSONString);
+
+            Console.WriteLine(c1);
+
+            Console.ReadKey();
+            
+
+
+    }
+    }
+}
