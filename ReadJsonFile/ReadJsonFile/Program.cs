e900b55ae552ef40987eb5d351439f868a2eed7b
shwethakulal <shwetha.hosangadi@gmail.com>
2018-01-05 11:56:45 +0530

    extracting json file
    extracting json file and display in console with exceptions

---
+++ ReadJsonFile/ReadJsonFile/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ReadJsonFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                using (StreamReader sr = new StreamReader(@"data\JsonData.json"))
+                {
+
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+
+                        Console.WriteLine(line);
+
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
